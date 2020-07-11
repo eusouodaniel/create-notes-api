38407c5749cd3140196cb72d337fc0c14d38d794
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-07-11 16:38:21 -0300

    NEW: get all notes and update function note

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -30,6 +30,36 @@ router.get('/:id', withAuth, async (req, res) => {
     }
 })
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+        let notes = await Note.find({ author: req.user._id });
+        res.send(notes)
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
+
+router.put('/:id', withAuth, async (req, res) => {
+    const { title, body } = req.body;
+    const { id } = req.params;
+    try {
+        let note = await Note.findById(id);
+        if(isOwner(req.user, note)){
+            let note = await Note.findOneAndUpdate(
+                {_id: id},
+                { $set: { title: title, body: body } },
+                { upsert: true, 'new': true }
+            )
+
+            res.json(note);
+        } else {
+            res.status(403).json({ error: "Permission denied" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error when update note" });
+    }
+})
+
 const isOwner = (user, note) => {
     if (JSON.stringify(user._id) == JSON.stringify(note.author._id)){
         return true;
